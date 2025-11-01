0f272e797e2ff3654ebee5498f7f2701528aa12b
Ivc <vank300828@gmail.com>
2025-11-01 22:25:24 +0100

    add eks resources, variables

---
+++ modules/eks/eks.tf
@@ -0,0 +1,126 @@
+# IAM role for the EKS control plane
+resource "aws_iam_role" "eks_cluster" {
+  name = "${var.cluster_name}-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
+  role       = aws_iam_role.eks_cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSVPCResourceController" {
+  role       = aws_iam_role.eks_cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+}
+
+# Security group for the cluster
+resource "aws_security_group" "eks_cluster_sg" {
+  name        = "${var.cluster_name}-cluster-sg"
+  description = "Cluster communication with worker nodes"
+  vpc_id      = var.vpc_id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(
+    {
+      Name = "${var.cluster_name}-sg"
+    },
+    var.tags,
+  )
+}
+
+# The EKS cluster
+resource "aws_eks_cluster" "this" {
+  name     = var.cluster_name
+  role_arn = aws_iam_role.eks_cluster.arn
+  version  = var.cluster_version
+
+  vpc_config {
+    subnet_ids              = concat(var.private_subnet_ids, var.public_subnet_ids)
+    security_group_ids      = [aws_security_group.eks_cluster_sg.id]
+    endpoint_private_access = true
+    endpoint_public_access  = true
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.eks_cluster_AmazonEKSVPCResourceController,
+  ]
+
+  tags = var.tags
+}
+
+# IAM role for nodes
+resource "aws_iam_role" "eks_nodegroup" {
+  name = "${var.cluster_name}-nodegroup-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "nodegroup_AmazonEKSWorkerNodePolicy" {
+  role       = aws_iam_role.eks_nodegroup.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "nodegroup_AmazonEC2ContainerRegistryReadOnly" {
+  role       = aws_iam_role.eks_nodegroup.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_role_policy_attachment" "nodegroup_AmazonEKS_CNI_Policy" {
+  role       = aws_iam_role.eks_nodegroup.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+# Managed node group
+resource "aws_eks_node_group" "this" {
+  cluster_name    = aws_eks_cluster.this.name
+  node_group_name = "${var.cluster_name}-ng"
+  node_role_arn   = aws_iam_role.eks_nodegroup.arn
+  subnet_ids      = var.private_subnet_ids
+  instance_types  = var.instance_types
+
+  scaling_config {
+    desired_size = var.node_group_desired_size
+    min_size     = var.node_group_min_size
+    max_size     = var.node_group_max_size
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  depends_on = [
+    aws_eks_cluster.this,
+    aws_iam_role_policy_attachment.nodegroup_AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.nodegroup_AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.nodegroup_AmazonEKS_CNI_Policy,
+  ]
+
+  tags = var.tags
+}
---
+++ modules/eks/variables.tf
@@ -0,0 +1,52 @@
+variable "cluster_name" {
+  type        = string
+  description = "EKS cluster name"
+  default     = "lesson-5-eks"
+}
+
+variable "cluster_version" {
+  type        = string
+  description = "Kubernetes version for EKS"
+  default     = "1.30"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "VPC where EKS will be created"
+}
+
+variable "private_subnet_ids" {
+  type        = list(string)
+  description = "Private subnets for EKS worker nodes"
+}
+
+variable "public_subnet_ids" {
+  type        = list(string)
+  description = "Public subnets (optionally) for load balancers"
+  default     = []
+}
+
+variable "node_group_desired_size" {
+  type    = number
+  default = 2
+}
+
+variable "node_group_min_size" {
+  type    = number
+  default = 2
+}
+
+variable "node_group_max_size" {
+  type    = number
+  default = 4
+}
+
+variable "instance_types" {
+  type    = list(string)
+  default = ["t3.medium"]
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
