9a96e9ea02466be7f6663038b166768c9882530b
Ivc <vank300828@gmail.com>
2025-11-04 20:03:29 +0100

    add eks permissions

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
-terraform {
-  backend "s3" {
-    bucket         = "lesson-7-tfstate-1761676973"
-    key            = "lesson-7/terraform.tfstate"
-    region         = "eu-central-1"
-    use_lockfile   = true
-    encrypt        = true
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket         = "lesson-7-tfstate-1761676973"
+#    key            = "lesson-7/terraform.tfstate"
+#    region         = "eu-central-1"
+#    use_lockfile   = true
+#    encrypt        = true
+#  }
+#}
