ea9aec549a252bb21e807ba5e5cbe4573db0abe6
Ivan <vank300828@gmail.com>
2025-11-09 01:49:06 +0100

    reconfigure with the new backend

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "lesson-7-tfstate-1761676973"
+    bucket         = "lesson-7-tfstate-hcwh1a"
     key            = "lesson-7/terraform.tfstate"
     region         = "eu-central-1"
     use_lockfile   = true
