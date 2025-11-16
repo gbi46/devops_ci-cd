b45ae2fb327391ac69ab945aea790d556c41c388
Ivan <vank300828@gmail.com>
2025-11-16 16:00:50 +0100

    edit s3 variables

--- modules/s3-backend/variables.tf
+++ modules/s3-backend/variables.tf
@@ -5,12 +5,3 @@ variable "bucket_name" {
 variable "table_name" {
   type = string
 }
-
-variable "dynamodb_name" {
-  type = string
-}
-
-variable "bucket_region" {
-  type    = string
-  default = "eu-central-1"
-}
