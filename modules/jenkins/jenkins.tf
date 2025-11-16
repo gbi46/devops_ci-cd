3f6bec0fdb67667046cdddbb495fd767b13a589b
Ivan <vank300828@gmail.com>
2025-11-16 21:08:27 +0100

    upd jenkins config

--- modules/jenkins/jenkins.tf
+++ modules/jenkins/jenkins.tf
@@ -6,10 +6,33 @@ resource "kubernetes_namespace" "jenkins" {
 
 resource "helm_release" "jenkins" {
   name       = "jenkins"
-  namespace  = kubernetes_namespace.jenkins.metadata[0].name
+  namespace  = "jenkins"
   repository = "https://charts.jenkins.io"
   chart      = "jenkins"
   version    = var.chart_version
+
+  set {
+    name  = "controller.admin.username"
+    value = "admin"
+  }
+
+  set {
+    name  = "controller.admin.password"
+    value = "admin123"
+  }
+
+  set {
+    name  = "controller.jenkinsUrl"
+    value = "http://jenkins.jenkins.svc.cluster.local:8080"
+  }
+
+  set {
+    name  = "controller.installPlugins"
+    value = ""
+  }
+
+  timeout = 900
+  wait    = true
 
   values = [
     file("${path.module}/values.yaml")
