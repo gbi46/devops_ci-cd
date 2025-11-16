a5dad5ba474bbc91ca83532283347fbfcc90d792
Ivan <vank300828@gmail.com>
2025-11-16 18:28:50 +0100

    upd eks vars

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -1,9 +1,6 @@
 variable "cluster_name" { type = string }
 variable "vpc_id" { type = string }
 variable "private_subnets" { type = list(string) }
-variable "cluster_name" {
-  type = string
-}
 
 variable "cluster_endpoint_public_access" {
   type    = bool
