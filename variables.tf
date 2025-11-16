02f6d2c48f9b964ee08c7678ce48d5b4546ab445
Ivan <vank300828@gmail.com>
2025-11-16 16:02:14 +0100

    add root variables

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  type    = string
+  default = "eu-central-1"
+}
+
+variable "tf_state_bucket_name" {
+  type    = string
+  default = "lesson-8-9-tf-state-bucket-1"
+}
+
+variable "tf_lock_table_name" {
+  type    = string
+  default = "terraform-locks"
+}
