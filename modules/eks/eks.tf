800142f5f8ae7b7a28ab2d82dfc115d37016738d
Ivan <vank300828@gmail.com>
2025-11-16 19:39:21 +0100

    switch config to eks_auth module

--- modules/eks/eks.tf
+++ modules/eks/eks.tf
@@ -12,17 +12,6 @@ module "eks" {
   cluster_endpoint_private_access      = var.cluster_endpoint_private_access
   cluster_endpoint_public_access_cidrs = var.cluster_endpoint_public_access_cidrs
 
-   # Manage aws-auth ConfigMap via module
-  manage_aws_auth = true
-
-  aws_auth_users = [
-    {
-      userarn  = "arn:aws:iam::534278686940:user/terraform-user"
-      username = "terraform-user"
-      groups   = ["system:masters"]
-    }
-  ]
-
   eks_managed_node_groups = {
     default = {
       desired_size = 2
@@ -32,4 +21,21 @@ module "eks" {
       instance_types = ["t3.medium"]
     }
   }
+}
+
+module "eks_auth" {
+  source  = "terraform-aws-modules/eks/aws//modules/aws-auth"
+  version = "~> 20.0"
+
+  manage_aws_auth_configmap = true
+
+  aws_auth_users = [
+    {
+      userarn  = "arn:aws:iam::534278686940:user/<YOUR_USERNAME>"
+      username = "terraform-user"
+      groups   = ["system:masters"]
+    }
+  ]
+
+  depends_on = [module.eks]
 }
