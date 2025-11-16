21ac75e807f597bedc5f61a426829a8c2a5535b0
Ivan <vank300828@gmail.com>
2025-11-16 12:31:22 +0100

    init

--- infrastructure/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "lesson-7-tfstate-hcwh1a"
-    key            = "lesson-7/terraform.tfstate"
-    region         = "eu-central-1"
-    use_lockfile   = true
-    encrypt        = true
-  }
-}
