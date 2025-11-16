cff9f8b380d77560cf6b1ed377bdf7ac5217ae3f
Ivan <vank300828@gmail.com>
2025-11-16 14:11:39 +0100

    add providers data

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "s3_backend" {
   source = "./modules/s3-backend"
 
-  bucket_name      = "my-tf-state-bucket"
+  bucket_name      = "lesson-8-9-tf-state-bucket"
   dynamodb_name    = "terraform-locks"
   bucket_region    = "eu-central-1"
 }
