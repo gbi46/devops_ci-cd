a12abc103d67c910d2e7f44fd5f54f244a25ea5a
Ivc <vank300828@gmail.com>
2025-11-20 19:55:04 +0100

    upd .gitignore

--- main.tf
+++ main.tf
@@ -40,27 +40,27 @@ locals {
   subnet_ids_first_two = slice(data.aws_subnets.in_default.ids, 0, 2)
 }
 
-module "rds_postgres" {
+module "rds" {
   source = "./modules/rds"
 
   use_aurora = false
-  name       = "demo-dev-postgres"
+  name       = "demo-postgres"
   vpc_id     = data.aws_vpc.default.id
   subnet_ids = local.subnet_ids_first_two
 
   engine         = "postgres"
-  engine_version = "14.20"
+  engine_version = "15.3"
   instance_class = "db.t3.micro"
   multi_az       = false
 
-  db_name         = "app"
-  master_username = "dbadmin"
+  db_name         = "app_db"
+  master_username = "app"
   master_password = var.db_password
 
   port                = 5432
   publicly_accessible = false
   # дозволь підключення зі свого IP, ЗАМІНИ на свій /32
-  ingress_cidr_blocks = ["95.90.200.245/32"]
+  ingress_cidr_blocks = ["1.2.3.4/32"]
 
   base_parameters = {
     max_connections = "50"
