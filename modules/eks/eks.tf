11c2677e9bc11fab30fe8b9ff061453f39c02609
Ivan <vank300828@gmail.com>
2025-11-21 13:44:22 +0100

    add aws_iam_role.node_role config

--- modules/eks/eks.tf
+++ modules/eks/eks.tf
@@ -36,3 +36,35 @@ resource "aws_eks_node_group" "default" {
     max_size     = 4
   }
 }
+
+data "aws_iam_policy_document" "node_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "node_role" {
+  name               = "${var.project_name}-eks-node-role"
+  assume_role_policy = data.aws_iam_policy_document.node_assume_role.json
+}
+
+# Стандартні політики для worker-нод EKS
+resource "aws_iam_role_policy_attachment" "node_AmazonEKSWorkerNodePolicy" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "node_AmazonEKS_CNI_Policy" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "node_AmazonEC2ContainerRegistryReadOnly" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
