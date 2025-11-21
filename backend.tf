11071d16a7d4d31df0aa7204a1f16740118b4a60
Ivan <vank300828@gmail.com>
2025-11-21 16:25:21 +0100

    upd aurora engine ver

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
-#terraform {
-#  backend "s3" {
-#    bucket         = "demo-platform-tf-state"
-#    key            = "terraform/state/infra.tfstate"
-#    region         = "eu-central-1"
-#    use_lockfile   = true
-#    encrypt        = true
-#  }
-#}
+terraform {
+  backend "s3" {
+    bucket         = "demo-platform-tf-state"
+    key            = "terraform/state/infra.tfstate"
+    region         = "eu-central-1"
+    use_lockfile   = true
+    encrypt        = true
+  }
+}
--- modules/rds/aurora.tf
+++ modules/rds/aurora.tf
@@ -1,6 +1,7 @@
 resource "aws_rds_cluster" "aurora" {
   cluster_identifier      = "${var.project_name}-aurora"
   engine                  = "aurora-postgresql"
+  engine_version          = "17.4"
   master_username         = var.db_username
   master_password         = var.db_password
   db_subnet_group_name    = aws_db_subnet_group.this.name
@@ -12,5 +13,5 @@ resource "aws_rds_cluster_instance" "aurora_instance" {
   identifier         = "${var.project_name}-aurora-${count.index}"
   cluster_identifier = aws_rds_cluster.aurora.id
   engine             = "aurora-postgresql"
-  instance_class     = "db.t3.small"
+  instance_class     = "db.t3.medium"
 }
