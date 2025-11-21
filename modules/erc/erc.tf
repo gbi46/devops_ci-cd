c5c7517dab224a220e1451a8ea7966dc3a9672c7
Ivan <vank300828@gmail.com>
2025-11-21 11:43:12 +0100

    add ecr module configs

---
+++ modules/erc/erc.tf
@@ -0,0 +1,15 @@
+variable "project_name" {}
+variable "repository_name" {}
+
+resource "aws_ecr_repository" "this" {
+  name                 = "${var.project_name}/${var.repository_name}"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+output "repository_url" {
+  value = aws_ecr_repository.this.repository_url
+}
