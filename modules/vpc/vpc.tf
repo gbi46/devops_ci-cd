dc9d88c8bf1db816b320cf105c055c01bde30472
Ivan <vank300828@gmail.com>
2025-11-21 11:42:32 +0100

    add vpc module configs

---
+++ modules/vpc/vpc.tf
@@ -0,0 +1,28 @@
+resource "aws_vpc" "this" {
+  cidr_block           = var.cidr_block
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "${var.project_name}-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.this.id
+}
+
+resource "aws_subnet" "public" {
+  count                   = length(var.azs)
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = cidrsubnet(var.cidr_block, 4, count.index)
+  map_public_ip_on_launch = true
+  availability_zone       = var.azs[count.index]
+}
+
+resource "aws_subnet" "private" {
+  count             = length(var.azs)
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = cidrsubnet(var.cidr_block, 4, count.index + 4)
+  availability_zone = var.azs[count.index]
+}
