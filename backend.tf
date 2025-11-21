cc4b2ee1f39d6c89af4f20e57c3fdf13e89c695b
Ivan <vank300828@gmail.com>
2025-11-21 13:16:53 +0100

    mv backend config to remote

--- backend.tf
+++ backend.tf
@@ -1,16 +1,15 @@
-#terraform {
-#  backend "s3" {
-#    bucket         = var.tf_state_bucket
-#    key            = "terraform/state/infra.tfstate"
-#    region         = var.region
-#    dynamodb_table = var.tf_state_dynamodb_table
-#    encrypt        = true
-#  }
-#}
-#
-#variable "tf_state_bucket" {}
-#variable "tf_state_dynamodb_table" {}
-#variable "region" {
-#  default = "eu-central-1"
-#}
-#
+terraform {
+  backend "s3" {
+    bucket         = var.tf_state_bucket
+    key            = "terraform/state/infra.tfstate"
+    region         = var.region
+    dynamodb_table = var.tf_state_dynamodb_table
+    encrypt        = true
+  }
+}
+
+variable "tf_state_bucket" {}
+variable "tf_state_dynamodb_table" {}
+variable "region" {
+  default = "eu-central-1"
+}
