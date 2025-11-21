796d98ecfaabc7121bcfd02a98d2bb9fe3a63d4a
Ivan <vank300828@gmail.com>
2025-11-21 11:39:39 +0100

    add outputs conf

---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "eks_cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "rds_endpoint" {
+  value = module.rds.db_endpoint
+}
+
+output "ecr_repo_url" {
+  value = module.ecr.repository_url
+}
