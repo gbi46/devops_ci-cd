4fbd65a36bafc65872d749d90b536ac1dccfe13b
Ivc <vank300828@gmail.com>
2025-11-29 11:54:17 +0100

    upd eks config

--- main.tf
+++ main.tf
@@ -72,10 +72,27 @@ module "ecr" {
 module "eks" {
   source = "./modules/eks"
 
+  cluster_name    = "demo-platform-eks"
+  cluster_version = "1.30"
+
   project_name   = "demo-platform"
   vpc_id         = module.vpc.vpc_id
   private_subnet_ids = module.vpc.private_subnet_ids
   public_subnet_ids  = module.vpc.public_subnet_ids
+
+  eks_managed_node_groups = {
+    default = {
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+
+      instance_types = ["t3.medium"]
+      capacity_type  = "ON_DEMAND"
+
+      # optional:
+      # disk_size = 50
+    }
+  }
 }
 
 # --- RDS / Aurora ---
