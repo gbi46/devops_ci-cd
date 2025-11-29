01d64f553602d66e79a887b01fe2bb091225096a
Ivc <vank300828@gmail.com>
2025-11-29 11:58:00 +0100

    add eks_managed_node_groups variable

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -2,3 +2,7 @@ variable "project_name" {}
 variable "vpc_id" {}
 variable "private_subnet_ids" { type = list(string) }
 variable "public_subnet_ids"  { type = list(string) }
+variable "eks_managed_node_groups" {
+  type    = any
+  default = {}
+}
